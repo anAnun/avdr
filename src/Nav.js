7ad8b355ab747af55e7aa66a2f089aa3430d1b0a
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-15 15:15:21 -0800

    nav test

--- src/Nav.js
+++ src/Nav.js
@@ -72,10 +72,6 @@ class Nav extends React.Component {
     });
   };
 
-  handle = () => {
-    this.props.history.push("/info");
-  };
-
   resume = () => {
     this.props.history.push("/resume");
   };
@@ -102,13 +98,14 @@ class Nav extends React.Component {
                   <span>Home</span>
                 </button>
               </a>
-              <button
-                onClick={() => this.handle()}
-                className="Home-nav-buttons-after"
-                id="Home-nav-buttons-after-id"
-              >
-                <span>Info</span>
-              </button>
+              <a href="/info" rel="noopener noreferrer">
+                <button
+                  className="Home-nav-buttons-after"
+                  id="Home-nav-buttons-after-id"
+                >
+                  <span>Info</span>
+                </button>
+              </a>
               <a href="/contact" rel="noopener noreferrer">
                 <button
                   className="Home-nav-buttons-after"
