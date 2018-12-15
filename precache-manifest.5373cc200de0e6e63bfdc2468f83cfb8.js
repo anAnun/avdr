ad23603ef740d9734ae03684faa59d264f35611c
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-15 03:16:25 -0800

    Updates

--- precache-manifest.5373cc200de0e6e63bfdc2468f83cfb8.js
+++ precache-manifest.5373cc200de0e6e63bfdc2468f83cfb8.js
@@ -8,19 +8,19 @@ self.__precacheManifest = [
     "url": "/avdr/static/js/runtime~main.fd32eeaf.js"
   },
   {
-    "revision": "973311a01917d7cf586c",
-    "url": "/avdr/static/js/main.973311a0.chunk.js"
+    "revision": "23de06f70eae7e44e4cf",
+    "url": "/avdr/static/js/main.23de06f7.chunk.js"
   },
   {
     "revision": "4eba2167ebf326e86679",
     "url": "/avdr/static/js/1.4eba2167.chunk.js"
   },
   {
-    "revision": "973311a01917d7cf586c",
+    "revision": "23de06f70eae7e44e4cf",
     "url": "/avdr/static/css/main.1aed0bfa.chunk.css"
   },
   {
-    "revision": "b0ab10f45b3df600cf48034cd643494c",
+    "revision": "71c0bdabc2a203526c32f19f61603da2",
     "url": "/avdr/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/avdr/precache-manifest.43aec87cdbc9feead06d67879d6227ad.js"
+  "/avdr/precache-manifest.5373cc200de0e6e63bfdc2468f83cfb8.js"
 );
 
 workbox.clientsClaim();
