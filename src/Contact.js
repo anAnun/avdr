59262e418612e52d6fe8bb3fdded4c537fd27ae1
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-14 23:19:24 -0800

    set up

---
+++ src/Contact.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Contact extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Contact;
---
+++ src/Info.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+class Info extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <div />
+      </React.Fragment>
+    );
+  }
+}
+
+export default Info;
