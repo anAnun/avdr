c81eacbbbfc5d80cf48fb32c54da59d939d17479
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-15 00:51:25 -0800

    Updates

---
+++ precache-manifest.05a4c32de41d670f423544c2ef505547.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca77e2f41c9f9eab65b403ebd9b04f5f",
+    "url": "/avdr/static/media/LA.ca77e2f4.jpg"
+  },
+  {
+    "revision": "fd32eeaf69bd07e74166",
+    "url": "/avdr/static/js/runtime~main.fd32eeaf.js"
+  },
+  {
+    "revision": "f7df32b88413697766ff",
+    "url": "/avdr/static/js/main.f7df32b8.chunk.js"
+  },
+  {
+    "revision": "4eba2167ebf326e86679",
+    "url": "/avdr/static/js/1.4eba2167.chunk.js"
+  },
+  {
+    "revision": "f7df32b88413697766ff",
+    "url": "/avdr/static/css/main.1aed0bfa.chunk.css"
+  },
+  {
+    "revision": "2292b88604b79ab6404cd699889f55bf",
+    "url": "/avdr/index.html"
+  }
+];
