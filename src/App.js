bf52e0bd1d4b367e0fd62df2c4a90fde3d5c6439
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-15 02:19:01 -0800

    nav test

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
     return (
       <React.Fragment>
         <div className="background-image-app" />
-        <HashRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename="https://ananun.github.io/avdr/">
           <Switch>
             <Route exact path="/" component={Home} />
             <Route exact path="/info" component={Info} />
