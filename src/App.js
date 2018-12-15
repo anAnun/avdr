b49fc39c67ad1c9e691ff7fe0a8c4a3563ebf34b
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-15 02:52:58 -0800

    nav test

--- src/App.js
+++ src/App.js
@@ -12,17 +12,9 @@ class App extends Component {
         <div className="background-image-app" />
         <HashRouter basename="https://ananun.github.io/avdr/#/">
           <Switch>
-            <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/info"}
-              component={Info}
-            />
-            <Route
-              exact
-              path={process.env.PUBLIC_URL + "/contact"}
-              component={Contact}
-            />
+            <Route exact path="/" component={Home} />
+            <Route exact path="/info" component={Info} />
+            <Route exact path="/contact" component={Contact} />
             <Route component={() => <div>404 Not found </div>} />
           </Switch>
         </HashRouter>
