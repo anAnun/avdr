b14cfedc563b724076151514f88310ab9c74e3ba
Allan Yeznaian <allanyeznaian@gmail.com>
2018-12-17 15:21:48 -0800

    rough draft

--- src/Contact.js
+++ src/Contact.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from "./Nav";
-import contact from "./images/contact.jpg";
 import "./Contact.css";
+import "./media600.css";
 
 class Contact extends React.Component {
   render() {
@@ -16,17 +16,32 @@ class Contact extends React.Component {
         <br />
         <div className="text-contact-container">
           <div className="text-contact">
-            <div className="align">Contact us!</div>
+            <br />
+            <div className="align" id="align">
+              Contact us!
+            </div>
+            <br />
           </div>
         </div>
-        <a href="tel:+1-818-982-7376">Office phone: 818-982-7376</a>
-        <div>fax: 818-982-2118</div>
-        <div>Address: 7361 Laurel Canyon Blvd North Hollywood CA 91605</div>
-        <div className="credit-photo">
-          Background Photo from{" "}
-          <a href="https://pngtree.com/freebackground/business-people-using-mobile-phones-definition-pictures_372268.html">
-            pngtree.com
-          </a>
+        <div className="contact-center">
+          <label>Office phone: </label>
+          <a href="tel:+1-818-982-7376">818-982-7376</a>
+          <div>fax: 818-982-2118</div>
+          <div>Address: 7361 Laurel Canyon Blvd North Hollywood CA 91605</div>
+        </div>
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <br />
+        <div className="credit-container">
+          <div className="credit-photo">
+            Background Photo from{" "}
+            <a href="https://pngtree.com/freebackground/business-people-using-mobile-phones-definition-pictures_372268.html">
+              pngtree.com
+            </a>
+          </div>
         </div>
       </React.Fragment>
     );
